c58e110d726480d955a2f176f82147cf6f3f958d
montacir ait lahcen <montaciraitlahcen@gmail.com>
2026-05-08 01:07:33 +0100

    feat: sticky navigation

--- public/script.js
+++ public/script.js
@@ -1,30 +1,32 @@
 "use strict";
-const btnScrollTo = document.querySelector(".btn--scroll-to");
-const firstSection = document.querySelector(".first_section");
-const navLinks = document.querySelector(".nav_links");
-const tabbedComponent = document.querySelector(".tabbed_component");
-btnScrollTo.addEventListener("click", () => {
-  firstSection.scrollIntoView({ behavior: "smooth" });
+const btnScrollToElement = document.querySelector(".btn--scroll-to");
+const firstSectionElement = document.querySelector("#first_section");
+const navLinksElement = document.querySelector(".nav_links");
+const tabbedComponentElement = document.querySelector(".tabbed_component");
+const navElement = document.querySelector(".nav");
+btnScrollToElement.addEventListener("click", () => {
+  firstSectionElement.scrollIntoView({ behavior: "smooth" });
 });
-// document.querySelectorAll('.nav_link').forEach(function (el) {
-//     el.addEventListener('click', function (e) {
-//         e.preventDefault();
-//         const id = this.getAttribute('href');
-//         document.querySelector(id).scrollIntoView({ behavior: 'smooth'});
-//     })
-// })
-navLinks.addEventListener("click", function (e) {
+document.querySelectorAll(".nav_link").forEach(function (el) {
+  el.addEventListener("click", function (e) {
+    e.preventDefault();
+    const id = this.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  });
+});
+navLinksElement.addEventListener("click", function (e) {
   e.preventDefault();
   const id = e.target.getAttribute("href");
   document.querySelector(id).scrollIntoView({ behavior: "smooth" });
 });
 
-//tabbed component
-tabbedComponent.addEventListener("click", function (e) {
+// tabbed component
+tabbedComponentElement.addEventListener("click", function (e) {
   const target = e.target.closest("button");
+  if (!target) return;
   const id = target.getAttribute("component");
   target.classList.add("translate-y-[-66%]");
-  [...tabbedComponent.children]
+  [...tabbedComponentElement.children]
     .filter(
       (el) => el.getAttribute("component") != id && el.tagName == "BUTTON",
     )
@@ -39,4 +41,30 @@ tabbedComponent.addEventListener("click", function (e) {
       el.classList.add("hidden");
     }
   });
+});
+
+navElement.addEventListener("mouseover", function (e) {
+  if (e.target.classList.contains("nav_link")) {
+    const target = e.target;
+    const siblings = target.closest(".nav").querySelectorAll(".nav_link");
+    const logo = target.closest(".nav").querySelector("#img");
+    siblings.forEach(el => {
+        if(el != target) el.classList.add('opacity-[.5]');
+    })
+    logo.classList.add('opacity-[.5]');
+  }
+});
+navElement.addEventListener("mouseout", function (e) {
+    const target = e.target;
+    const siblings = target.closest(".nav").querySelectorAll(".nav_link");
+    const logo = target.closest(".nav").querySelector("#img");
+    siblings.forEach((el) => {
+      if (el != target) el.classList.remove("opacity-[.5]");
+    });
+    logo.classList.remove("opacity-[.5]");
+});
+const firstSectionTop = firstSectionElement.getBoundingClientRect().top;
+window.addEventListener('scroll', function() {
+    if(window.scrollY > firstSectionTop) navElement.classList.add('Sticky');
+    else navElement.classList.remove('Sticky');
 });
